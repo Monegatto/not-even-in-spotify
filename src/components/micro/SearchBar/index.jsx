ee62026fe80072a1ab5c577007b962b60ac85c8c
Giovani Ohira <giovanirossi863@gmail.com>
2024-11-15 16:16:04 -0300

    add search functionality

--- src/components/micro/SearchBar/index.jsx
+++ src/components/micro/SearchBar/index.jsx
@@ -1,12 +1,36 @@
 import './SearchBar.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
-function SearchBar() {
+function SearchBar({ handleSearch }) {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch(inputValue);
+    }
+  };
+
+  const handleIconClick = () => {
+    handleSearch(inputValue);
+  };
+
   return (
-    <div>
-      <input type="text" placeholder="Paste a song URL to see more details" />
-      <FontAwesomeIcon icon={faMagnifyingGlass} color='#1E1E1E' className='find-icon'/>
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Paste a song URL to see more details"
+        value={inputValue}  
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <FontAwesomeIcon
+        icon={faMagnifyingGlass}
+        color="#1E1E1E"
+        className="find-icon"
+        onClick={handleIconClick}
+      />
     </div>
   );
 }
