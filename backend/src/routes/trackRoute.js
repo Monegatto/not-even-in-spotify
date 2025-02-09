b4b25b14a39b6a935d65247f6ab7397fcb013345
Giovani Ohira <giovanirossi863@gmail.com>
2025-02-09 10:04:31 -0300

    add auth to track routes

--- backend/src/routes/trackRoute.js
+++ backend/src/routes/trackRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const trackController = require('../controllers/trackController');
+const authenticateToken = require('../middlewares/authenticateToken');
 
 const router = express.Router();
 
-router.get('/track/:trackId', trackController.getTrackInfo);
+router.get('/track/:trackId', authenticateToken, trackController.getTrackInfo);
 
 module.exports = router;
