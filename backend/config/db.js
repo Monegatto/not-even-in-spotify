a29bd271357fd73b91669ae3343266f6c364c187
Giovani Ohira <giovanirossi863@gmail.com>
2025-02-12 21:41:32 -0300

    update spotify and DB configs

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,11 @@
-const { Pool } = require('pg');
-require ('dotenv').config();
+// config/db.js
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'postgres',
+  logging: false, // Desativa logs SQL
 });
 
-module.exports = pool;
+module.exports = { sequelize };
